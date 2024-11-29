3927645b14c888313b327e22459d7ff0b06c6d73
Raka Agus Maulana <kakakunyukya@gmail.com>
2024-11-29 14:09:26 +0700

    Add: Add View Binding To class

--- app/src/main/java/com/artforyou/testcodesoulparking/view/add_detail/AddAndDetailActivity.kt
+++ app/src/main/java/com/artforyou/testcodesoulparking/view/add_detail/AddAndDetailActivity.kt
@@ -6,16 +6,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.artforyou.testcodesoulparking.R
+import com.artforyou.testcodesoulparking.databinding.ActivityAddAndDetailBinding
 
 class AddAndDetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddAndDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_add_and_detail)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        binding = ActivityAddAndDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- app/src/main/java/com/artforyou/testcodesoulparking/view/splashscreen/SplashScreenActivity.kt
+++ app/src/main/java/com/artforyou/testcodesoulparking/view/splashscreen/SplashScreenActivity.kt
@@ -6,16 +6,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.artforyou.testcodesoulparking.R
+import com.artforyou.testcodesoulparking.databinding.ActivitySplashScreenBinding
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_splash_screen)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
