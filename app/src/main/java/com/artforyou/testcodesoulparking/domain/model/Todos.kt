85c36d10255fea7b79c6f2594a71339122dbdf3b
Raka Agus Maulana <kakakunyukya@gmail.com>
2024-11-29 13:45:39 +0700

    Add: Update Todos.kt

--- app/src/main/java/com/artforyou/testcodesoulparking/domain/model/Todos.kt
+++ app/src/main/java/com/artforyou/testcodesoulparking/domain/model/Todos.kt
@@ -3,6 +3,7 @@ package com.artforyou.testcodesoulparking.domain.model
 data class Todos(
     val id: Int,
     val title: String,
-    val description: String,
-    val date: String
+    val description: String = "",
+    val date: String,
+    val isTrash: Boolean
 )
