a879816e3907efc0e77b063110645ad1d4760e27
Raka Agus Maulana <kakakunyukya@gmail.com>
2024-12-01 16:18:50 +0700

    Chord: Add Color, Add Date utils, and Theme

--- app/src/main/java/com/artforyou/testcodesoulparking/utils/DateUtils.kt
+++ app/src/main/java/com/artforyou/testcodesoulparking/utils/DateUtils.kt
@@ -1,4 +1,18 @@
 package com.artforyou.testcodesoulparking.utils
 
+import java.text.SimpleDateFormat
+import java.util.Locale
+
 object DateUtils {
+    fun formatDate(dateStr: String, originalFormat: String = "yyyy-MM-dd HH:mm:ss", targetFormat: String = "dd MMM yyyy"): String? {
+        return try {
+            val originalFormatter = SimpleDateFormat(originalFormat, Locale.getDefault())
+            val targetFormatter = SimpleDateFormat(targetFormat, Locale.getDefault())
+            val date = originalFormatter.parse(dateStr)
+            date?.let { targetFormatter.format(it) }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
+    }
 }
